766331a056a493fe4dcb57527e7847dfa2d498bd
Karn <zeropro500@gmail.com>
2019-02-13 21:59:13 +0700

    'modifypdm'

--- web/scripts/controllers/product-milk-detail/MainPMDController.js
+++ web/scripts/controllers/product-milk-detail/MainPMDController.js
@@ -1,22 +1,22 @@
-angular.module('e-homework').controller('MainPMDController', function($scope, $cookies, $filter, $state, $uibModal, HTTPService, IndexOverlayFactory) {
-	//console.log('Hello !');
+angular.module('e-homework').controller('MainPMDController', function ($scope, $cookies, $filter, $state, $uibModal, HTTPService, IndexOverlayFactory) {
+    //console.log('Hello !');
     $scope.DEFAULT_LANGUAGE = 'TH';
     $scope.menu_selected = 'management';
     var $user_session = sessionStorage.getItem('user_session');
-    
-    if($user_session != null){
+
+    if ($user_session != null) {
         $scope.$parent.currentUser = angular.fromJson($user_session);
-    }else{
-       window.location.replace('#/guest/logon');
+    } else {
+        window.location.replace('#/guest/logon');
     }
 
-    $scope.$parent.Menu = angular.fromJson(sessionStorage.getItem('menu_session'));    
+    $scope.$parent.Menu = angular.fromJson(sessionStorage.getItem('menu_session'));
 
-    $scope.loadList = function(action){
-        var params = {'condition' : $scope.condition};
+    $scope.loadList = function (action) {
+        var params = {'condition': $scope.condition};
         IndexOverlayFactory.overlayShow();
-        HTTPService.clientRequest(action, params).then(function(result){
-            if(result.data.STATUS == 'OK'){
+        HTTPService.clientRequest(action, params).then(function (result) {
+            if (result.data.STATUS == 'OK') {
                 $scope.List = result.data.DATA.List;
                 // console.log($scope.UserList);
             }
@@ -24,19 +24,21 @@ angular.module('e-homework').controller('MainPMDController', function($scope, $c
         });
     }
 
-    $scope.getThaiDate = function(date){
+    $scope.getThaiDate = function (date) {
         // console.log(date);
         return convertDateToFullThaiDate(new Date(date));
     }
 
-    $scope.goUpdate = function(detailid){
-        window.location.href = '#/product-milk-detail/update/' + detailid+'-xx';
+    $scope.goUpdate = function (detailid) {
+        window.location.href = '#/product-milk-detail/update/' + detailid + '-xx';
+    }
+    $scope.cancelUpdate = function () {
+        window.location.href = '#/subproduct-milk';
     }
 
-   
 
 
-   
+
     $scope.loadList('product-milk-detail/list', '');
 
 
